30f26bf92e4c3b42329a53b670188353a86429df
briellydiassilva <146475496+briellydiassilva@users.noreply.github.com>
2024-11-13 16:35:54 -0300

    .

--- aula-13-11/index.php
+++ aula-13-11/index.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>Document</title>
 </head>
 <body>
-    ola
+    <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#home">News</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#about">About</a>
+        </li>
+    </ul>
+    <p> 2AII</p>
 </body>
 </html>
