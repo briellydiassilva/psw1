7b6f86b9b673e32c74bf84b4508e113147328dc1
briellydiassilva <146475496+briellydiassilva@users.noreply.github.com>
2024-11-22 02:45:31 -0300

    .

---
+++ atv02-gabrielly-dias-19-11/index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>atv2 psw</title>
+</head>
+<body>
+
+<header>    
+        <img class="imgtopo" src="https://i.ytimg.com/vi/lsUyymMqtcs/maxresdefault_live.jpg" alt="">
+        <h1>FORMULÁRIO DE PSW</h1>
+       <img class="imgtopo" src="https://th.bing.com/th/id/OIP.VjNGJyEOkEkdIiaQrMMY7gHaHa?rs=1&pid=ImgDetMain" alt="">
+       
+    </header>
+
+</div>
+
+      <ul>
+        <li><a href="#formulario">Formulário</a></li>
+        <li><a href="#contato">Contato</a></li>
+        </li>
+    </ul>
+
+  <div class="imagem">
+  <img src="img/WhatsApp Image 2024-11-06 at 12.25.29.jpeg" alt="">
+  </div>
+  </div>
+
+  <img src="img_chania.jpg" alt="">
+
+  <form action="/action_page.php">
+  <label for="fname">Nome completo:</label><br>
+  <input type="text" id="fname" name="fname" placeholder="Escreva seu nome..." required><br><br>
+
+  <label for="pwd">Senha:</label><br>
+  <input type="password" id="pwd" name="pwd" required><br><br>
+
+  <label for="data_de_nascimento">Data de nascimento:</label><br>
+  <input type="date" id="data_de_nascimento" name="data_de_nascimento" required><br><br>
+
+  <label for="phone">Telefone:</label><br>
+  <input type="tel" id="phone" name="telefone" placeholder="(xx) xxxxx-xxxx" pattern="\([0-9]{2}\) [0-9]{5}-[0-9]{4}" required><br><br>
+
+  <label for="email">Email:</label><br>
+  <input type="email" id="email" name="email" required><br><br>
+
+  <label for="fname">Endereço:</label><br>
+  <input type="text" id="fname" name="fname" placeholder="Escreva seu endereço..." required><br><br>
+
+  <p>Qual seu sexo:</p>
+  <input type="radio" id="feminino" name="sexo" value="Feminino">
+  <label for="feminino">Feminino</label><br>
+  <input type="radio" id="masculino" name="sexo" value="Masculino">
+  <label for="masculino">Masculino</label><br><br>
+
+  <p>Estado Civil:</p>
+  <input type="radio" id="Solteiro" name="estado civil" value="Solteiro">
+  <label for="solteiro">Solteiro(a)</label><br>
+  <input type="radio" id="Namorando" name="estado civil" value="namorando">
+  <label for="namorando">Namorando</label><br>
+  <input type="radio" id="Casado" name="estado civil" value="casado">
+  <label for="casado">Casado(a)</label><br><br>
+
+  <p>Qual seu esporte favorito?</p>
+
+  <form action="/action_page.php">
+  <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+  <label for="vehicle1"> Futsal e futebol</label><br>
+  <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
+  <label for="vehicle2"> Vôlei</label><br>
+  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+  <label for="vehicle3"> Handebol</label><br><br>
+
+<form action="/action_page.php">
+  <label for="myfile">Selecione um arquivo:</label><br>
+  <input type="file" id="myfile" name="myfile"><br><br>
+
+<form action="/action_page.php">
+  <label for="quantity">Quantidade de motos:(entre 0 e 10)</label><br>
+  <input type="number" id="quantity" name="quantity" min="0" max="10"><br><br>
+
+  <label for="l1">Quantidade de automóveis em casa (0 a 5):</label><br>
+  <input type="range" id="l1" name="quantidade" min="0" max="5"><br><br>
+
+  <p>Campo de pesquisa:</p>
+<form action="/action_page.php">
+  <input type="search" id="gsearch" name="gsearch"><br><br>
+
+  
+<p>Selecione um horário:</p>
+<form action="/action_page.php">
+  <input type="time" id="appt" name="appt"><br><br>
+
+  <p>Adicione sua página inicial:</p>
+  <form action="/action_page.php">
+  <input type="url" id="homepage" name="homepage"><br><br>
+
+  <p>Selecione uma semana:</p>
+<form action="/action_page.php">
+  <input type="week" id="week" name="week"><br><br>
+
+  <input type="reset" id="r6" value="Voltar">
+  <input type="submit" id="r7" value="Enviar">
+</form>
+
+<div class id ="footer">
+<div id="r1"><p>&copy; 2024 - TODOS OS DIREITOS RESERVADOS A GABRIELLY DIAS DA SILVA</p></div>
+
+</body>
+</html>
